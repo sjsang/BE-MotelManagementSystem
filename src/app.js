0d877dcd9e25a563df18c723d818ecefaef815c5
sjsang <sangnt6604@gmail.com>
2026-05-28 00:48:26 +0700

    CURD room

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./modules/auth/auth.routes');
+const roomRoutes = require('./modules/room/room.routes');
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/rooms', roomRoutes);
 
 module.exports = app;
